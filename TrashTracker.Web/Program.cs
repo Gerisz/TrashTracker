ea17aba7c9c8feb82d247ae93c62b4ba87249b41
gerisz <kiszely.geri@gmail.com>
2024-02-22 12:54:42 +0100

    fix homepage not loading

--- TrashTracker.Web/Program.cs
+++ TrashTracker.Web/Program.cs
@@ -33,10 +33,16 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllers();
 
