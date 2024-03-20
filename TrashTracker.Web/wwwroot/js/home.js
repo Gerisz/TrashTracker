6d7667a8936b902a83bc945910bdbc22fa613e71
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-03-20 22:25:39 +0100

    defined fill colors by status

--- TrashTracker.Web/wwwroot/js/home.js
+++ TrashTracker.Web/wwwroot/js/home.js
@@ -1,7 +1,22 @@
-﻿const defaultCenter = ol.proj.transform([19.0402635, 47.4978867], 'EPSG:4326', 'EPSG:3857');
+﻿const defaultCenter = ol.proj.transform([19.503736, 47.180086], 'EPSG:4326', 'EPSG:3857');
+const statusFills = {
+    Cleaned: new ol.style.Fill({
+        color: '#0000FF'
+    }),
+    Less: new ol.style.Fill({
+        color: '#00FF00'
+    }),
+    More: new ol.style.Fill({
+        color: '#FF0000'
+    }),
+    StillHere: new ol.style.Fill({
+        color: '#FFFF00'
+    })
+};
 
 var map = new ol.Map({
-    target: 'map',
+    controls: ol.control.defaults.defaults()
+        .extend([new ol.control.FullScreen()]),
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
@@ -10,9 +25,29 @@ var map = new ol.Map({
             source: new ol.source.Vector({
                 format: new ol.format.GeoJSON(),
                 url: './OnMap'
+            }),
+            style: new ol.style.Style({
+                image: new ol.style.Circle({
+                    fill: new ol.style.Fill({
+                        color: 'rgba(255,255,255,0.5)'
+                    }),
+                    stroke: new ol.style.Stroke({
+                        color: '#00FFFF',
+                        width: 1.25,
+                    }),
+                    radius: 5,
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(255,255,255,0.5)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: '#00FFFF',
+                    width: 1.25,
+                }),
             })
         })
     ],
+    target: 'map',
     view: new ol.View({
         center: defaultCenter,
         zoom: 7,
