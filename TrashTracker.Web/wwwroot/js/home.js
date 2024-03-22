bdcc1eeaa2668afa866b0618b39db0a4b493be26
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-03-22 16:24:50 +0100

    points in no cluster retrains original style

--- TrashTracker.Web/wwwroot/js/home.js
+++ TrashTracker.Web/wwwroot/js/home.js
@@ -1,5 +1,18 @@
 ﻿const defaultCenter = ol.proj.transform([19.503736, 47.180086], 'EPSG:4326', 'EPSG:3857');
-const fillStatuses = { Cleaned: '#198754', Less: '#FFC107', More: '#DC3545', StillHere: '#FD7E14' };
+const fillStatuses = {
+    Cleaned: new ol.style.Fill({
+        color: '#198754'
+    }),
+    Less: new ol.style.Fill({
+        color: '#FFC107'
+    }),
+    More: new ol.style.Fill({
+        color: '#DC3545'
+    }),
+    StillHere: new ol.style.Fill({
+        color: '#FD7E14'
+    })
+};
 const sizeRadiuses = { Bag: 6, Wheelbarrow: 8, Car: 10 };
 const styleCache = {};
 
@@ -20,28 +33,47 @@ var trashTrackerMap = new ol.Map({
             style: function (feature) {
                 const size = feature.get('features').length;
                 let style = styleCache[size];
-                if (!style) {
-                    style = new ol.style.Style({
-                        image: new ol.style.Circle({
-                            fill: new ol.style.Fill({
-                                color: '#888888'
+                if (size > 1) {
+                    if (!style) {
+                        style = new ol.style.Style({
+                            image: new ol.style.Circle({
+                                fill: new ol.style.Fill({
+                                    color: '#888888'
+                                }),
+                                stroke: new ol.style.Stroke({
+                                    color: '#000000',
+                                    width: 1
+                                }),
+                                radius: 8
                             }),
-                            stroke: new ol.style.Stroke({
-                                color: '#000000',
-                                width: 1
-                            }),
-                            radius: 8
-                        }),
-                        text: new ol.style.Text({
-                            text: size != 1 ? size.toString() : "",
-                            fill: new ol.style.Fill({
-                                color: '#FFFFFF',
-                            }),
-                        })
-                    });
-                    styleCache[size] = style;
+                            text: new ol.style.Text({
+                                text: size != 1 ? size.toString() : "",
+                                fill: new ol.style.Fill({
+                                    color: '#FFFFFF',
+                                }),
+                            })
+                        });
+                        styleCache[size] = style;
+                    }
+                    return style;
                 }
-                return style;
+                const point = feature.get('features')[0];
+                return new ol.style.Style({
+                    image: new ol.style.Circle({
+                        fill: fillStatuses[point.get('status')],
+                        stroke: new ol.style.Stroke({
+                            color: '#000000',
+                            width: 1
+                        }),
+                        radius: sizeRadiuses[point.get('size')]
+                    }),
+                    text: new ol.style.Text({
+                        text: size != 1 ? size.toString() : "",
+                        fill: new ol.style.Fill({
+                            color: '#FFFFFF',
+                        }),
+                    })
+                });
             }
         })
     ],
