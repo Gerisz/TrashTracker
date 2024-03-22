a9af43be4c349198c53280f2cbb9fe19c00796db
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-03-22 19:45:08 +0100

    :)

---
+++ TrashTracker.Data/Models/DTOs/Out/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace TrashTracker.Data.Models.DTOs.Out
+{
+    public class Error
+    {
+        public String? RequestId { get; set; }
+        public Boolean ShowRequestId => !String.IsNullOrEmpty(RequestId);
+    }
+}
