a655b1315eb672f7e991060fd224404a154e9803
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-03-26 13:02:06 +0100

    implemeted switch script for theme

--- TrashTracker.Web/wwwroot/js/site.js
+++ TrashTracker.Web/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(function () {
+    var toggler = document.querySelector('#theme-switch'),
+        root = document.getElementById('html'),
+        currentTheme = document.getElementById('html').getAttribute('data-bs-theme');
+
+    if (currentTheme == 'light')
+        toggler.removeAttribute('checked');
+    else
+        toggler.checked = 'true';
+
+    root.setAttribute('data-bs-theme', currentTheme);
+
+    toggler.addEventListener('change', toggleTheme);
+
+    function toggleTheme() {
+        if (this.checked) {
+            root.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+
+        else {
+            root.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('theme', 'light');
+
+        }
+    }
+})();
