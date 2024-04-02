50c06713ec38111e7695499b0b8a8c1bf29da526
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-04-02 12:15:37 +0200

    added example popover and offcanvas

--- TrashTracker.Web/wwwroot/js/home.js
+++ TrashTracker.Web/wwwroot/js/home.js
@@ -13,12 +13,56 @@ const fillStatuses = {
         color: '#FD7E14'
     })
 };
+const overlay = new ol.Overlay({
+    element: document.getElementById('popup'),
+    autoPan: {
+        animation: {
+            duration: 250
+        }
+    }
+});
 const sizeRadiuses = { Bag: 6, Wheelbarrow: 8, Car: 10 };
 const styleCache = {};
 
-var trashTrackerMap = new ol.Map({
+/*closer.onclick = function () {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+};*/
+
+class RotateNorthControl extends ol.control.Control {
+    /**
+     * @param {Object} [opt_options] Control options.
+     */
+    constructor(opt_options) {
+        const options = opt_options || {};
+
+        const button = document.createElement('button');
+        button.innerHTML = 'N';
+
+        const element = document.createElement('div');
+        element.className = 'rotate-north ol-unselectable ol-control';
+        element.appendChild(button);
+
+        super({
+            element: element,
+            target: options.target,
+        });
+
+        button.addEventListener('click', this.handleRotateNorth.bind(this), false);
+    }
+
+    handleRotateNorth() {
+        this.getMap().getView().setRotation(0);
+    }
+}
+
+let map = new ol.Map({
     controls: ol.control.defaults.defaults()
-        .extend([new ol.control.FullScreen()]),
+        .extend([
+            new ol.control.FullScreen(),
+            new RotateNorthControl()
+        ]),
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM()
@@ -77,9 +121,44 @@ var trashTrackerMap = new ol.Map({
             }
         })
     ],
+    overlays: [overlay],
     target: 'map',
     view: new ol.View({
         center: defaultCenter,
         zoom: 7,
     }),
+});
+
+const element = overlay.getElement();
+map.on('click', function (evt) {
+    let features;
+    try {
+        features = map.getFeaturesAtPixel(evt.pixel)[0].get('features');
+    } catch { return; }
+    if (features.length == 1) {
+        const feature = features[0];
+        const coordinate = feature.getGeometry().getCoordinates();
+        const hdms = ol.coordinate.toStringHDMS(ol.proj.toLonLat(coordinate));
+        const id = feature.get('id');
+        overlay.setPosition(coordinate);
+        let popover = bootstrap.Popover.getInstance(element);
+        if (popover)
+            popover.dispose();
+        popover = new bootstrap.Popover(element, {
+            animation: false,
+            container: element,
+            content: `<p>
+                          ${hdms}
+                      </p>
+                      <a class="btn btn-info" data-bs-title="Részletek" data-bs-toggle="tooltip"
+                              href="/Trashes/Details/${id}">
+                          <i class="fa-solid fa-circle-info" aria-hidden="true"></i>
+                          <span>Részletek</span>
+                      </a>`,
+            html: true,
+            placement: 'top',
+            title: `${id}. szemétpont`,
+        });
+        popover.show();
+    }
 });
--- TrashTracker.Web/wwwroot/js/site.js
+++ TrashTracker.Web/wwwroot/js/site.js
@@ -3,8 +3,8 @@
 
 // Write your JavaScript code.
 
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 (function () {
     var toggler = document.querySelector('#theme-switch'),
