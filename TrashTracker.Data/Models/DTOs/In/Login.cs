c7b842a05bdbea69b31aaf3e168281edfd556bc9
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-04-20 20:28:10 +0200

    add DTOs for login and register

---
+++ TrashTracker.Data/Models/DTOs/In/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashTracker.Data.Models.DTOs.In
+{
+    public class Login
+    {
+        [DisplayName("Név")]
+        public string UserName { get; set; } = null!;
+
+        [DisplayName("Jelszó")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ TrashTracker.Data/Models/DTOs/In/Register.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashTracker.Data.Models.DTOs.In
+{
+    public class Register
+    {
+        [DisplayName("Név")]
+        public string UserName { get; set; } = null!;
+
+        [DisplayName("Jelszó")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [DisplayName("Jelszó megerősítése")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string PasswordRepeat { get; set; } = null!;
+    }
+}
