670d95dda32373a420494bb2c67c6c57d1525fc9
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-04-21 20:49:04 +0200

    add ingnored file to source control

---
+++ TrashTracker.Data/Models/DTOs/Out/UserDetails.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using TrashTracker.Data.Models.Tables;
+
+namespace TrashTracker.Data.Models.DTOs.Out
+{
+    public class UserDetails
+    {
+        public String UserName { get; set; } = null!;
+        public String Email { get; set; } = null!;
+        public Boolean EmailConfirmed { get; set; }
+        public static Expression<Func<TrashTrackerUser, UserDetails>> Projection
+            (String imageUrlBase) => user => new UserDetails()
+            {
+                UserName = user.UserName!,
+                Email = user.Email!,
+                EmailConfirmed = user.EmailConfirmed
+            };
+
+        public static TrashDetails Create(TrashTrackerUser user, String imageUrlBase)
+            => Projection(imageUrlBase).Compile().Invoke(user);
+
+    }
+}
