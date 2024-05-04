5e6fcf095399051bd923b06e848e84919cc8fa11
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-05-04 12:03:03 +0200

    add validation to login and register dto

--- TrashTracker.Data/Models/DTOs/In/Login.cs
+++ TrashTracker.Data/Models/DTOs/In/Login.cs
@@ -6,10 +6,12 @@ namespace TrashTracker.Data.Models.DTOs.In
     public class Login 
     {
         [DisplayName("Felhasználónév vagy E-mail cím")]
+        [Required(ErrorMessage = "Felhasználónév megadása kötelező!")]
         public String UserName { get; set; } = null!;
 
         [DataType(DataType.Password)]
         [DisplayName("Jelszó")]
+        [Required(ErrorMessage = "Jelszó megadása kötelező!")]
         public String Password { get; set; } = null!;
     }
 }
--- TrashTracker.Data/Models/DTOs/In/Register.cs
+++ TrashTracker.Data/Models/DTOs/In/Register.cs
@@ -5,23 +5,33 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TrashTracker.Data.Models.DTOs.In
 {
-    public class Register 
+    public class Register
     {
         [DisplayName("Felhasználónév")]
+        [Required(ErrorMessage = "Felhasználónév megadása kötelező!")]
         public String UserName { get; set; } = null!;
 
         [DataType(DataType.EmailAddress)]
         [DisplayName("E-mail cím")]
         [EmailAddress]
+        [Required(ErrorMessage = "E-mail cím megadása kötelező!")]
         public String Email { get; set; } = null!;
 
         [DataType(DataType.Password)]
         [DisplayName("Jelszó")]
+        [Required(ErrorMessage = "Jelszó megadása kötelező!")]
+        [StringLength(32,
+            ErrorMessage = "A jelszó nem lehet rövidebb {2} és hosszabb {1} karakternél",
+            MinimumLength = 6)]
         public String Password { get; set; } = null!;
 
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Nem egyezik meg a megadott jelszóval!")]
         [DataType(DataType.Password)]
         [DisplayName("Jelszó megerősítése")]
+        [Required(ErrorMessage = "Jelszó megerősítése kötelező!")]
+        [StringLength(32,
+            ErrorMessage = "A jelszó nem lehet rövidebb {2} és hosszabb {1} karakternél",
+            MinimumLength = 6)]
         public String PasswordRepeat { get; set; } = null!;
 
         [DisplayName("Profilkép")]
