896d2f08d02e41476c12b1508e4a310ed66377cc
Kiszely Gergő János <d4cx8o@INF.ELTE.HU>
2024-05-21 16:00:27 +0200

    update docs and add ignored file

---
+++ TrashTracker.Data/Models/DTOs/Out/UserIndex.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Linq.Expressions;
+using TrashTracker.Data.Models.Tables;
+
+namespace TrashTracker.Data.Models.DTOs.Out
+{
+    public class UserIndex
+    {
+        public String Id { get; set; } = null!;
+
+        [DisplayName("Felhasználónév")]
+        public String UserName { get; set; } = null!;
+
+        [DisplayName("E-mail cím")]
+        public String Email { get; set; } = null!;
+
+        [DisplayName("Regisztrálás ideje")]
+        public DateTime RegistrationTime { get; set; }
+
+        public static Expression<Func<TrashTrackerUser, UserIndex>> Projection { get; }
+            = user => new UserIndex()
+            {
+                Id = user.Id,
+                UserName = user.UserName!,
+                Email = user.Email!,
+                RegistrationTime = user.RegistrationTime
+            };
+    }
+}
